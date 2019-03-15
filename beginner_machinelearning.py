84f89d51bf44a49138f27a6ff04a226a76c6aef2
xinyig97 <xinyig97@gmail.com>
2019-03-15 12:34:26 -0500

    prediction using linear regression

--- beginner_machinelearning.py
+++ beginner_machinelearning.py
@@ -4,10 +4,15 @@
 import pandas as pd 
 import quandl 
 import math
+import datetime
 import numpy as np 
 from sklearn import preprocessing, svm 
 from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.linear_model import LinearRegression 
+import matplotlib.pyplot as plt 
+from matplotlib import style
+
+style.use('ggplot')
 
 
 df = quandl.get("WIKI/GOOGL")
@@ -31,26 +36,64 @@ print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-X = np.array(df.drop(['label'],1)) # data uses captial X 
-y = np.array(df['label'])
+# X = np.array(df.drop(['label'],1)) # data uses captial X 
+# y = np.array(df['label'])
 
+# X = preprocessing.scale(X) # scale all values 
+# y = np.array(df['label'])
+
+X = np.array(df.drop(['label'],1)) # data feature captial X 
 X = preprocessing.scale(X) # scale all values 
-y = np.array(df['label'])
+X = X[:-forecast_out]
+
+X_lately = X[-forecast_out:] # we need to predict against, we dont have y value for 
+
+
+
+df.dropna(inplace=True)
+y = np.array(df[:-forecast_out]['label'])
+# y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
 
 
-# can use multiple treads and parallel programming!! n_jobs yea!!!
-# classifier= LinearRegression()
-# classifier.fit(X_train,y_train)
-# accuracy = classifier.score(X_test,y_test)
-
-
-classifier= svm.SVR(kernel = 'poly') # what is a kernel 
+#can use multiple treads and parallel programming!! n_jobs yea!!!
+classifier= LinearRegression()
 classifier.fit(X_train,y_train)
 accuracy = classifier.score(X_test,y_test)
 
-print(accuracy)
+
+# classifier= svm.SVR(kernel = 'poly') # what is a kernel 
+# classifier.fit(X_train,y_train)
+# accuracy = classifier.score(X_test,y_test)
+
+forecast_set = classifier.predict(X_lately) # predict unknow future values
+print(forecast_set,accuracy,forecast_out)
+
+df['Forest'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i] #df.loc refers the index , next_date is a date stamp that is the index of the dataframe 
+
+print(df.head()) # features at top
+print(df.tail()) # predicted data at the end such that only foret is there
+
+df['Adj. Close'].plot()
+df['Forest'].plot()
+plt.legend(loc = 4)
+plt.xlabel('date')
+plt.ylabel('price')
+plt.show()
+
+
+
+#print(accuracy)
 
 
 

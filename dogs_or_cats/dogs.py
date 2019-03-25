fb2e059bcce0d5f9f5691964bee14d8469544cd3
xinyig97 <xinyig97@gmail.com>
2019-03-25 16:54:30 -0500

    actually resume at 64 mid point after processing test data

--- dogs_or_cats/dogs.py
+++ dogs_or_cats/dogs.py
@@ -4,7 +4,7 @@ import os
 from random import shuffle
 from tqdm import tqdm
 
-
+# using cnn
 
 TRAIN_DIR = ' ' # path of train data 
 TEST_DIR  = ' '# path of test data 
@@ -29,3 +29,15 @@ def create_train_data():
     shuffle(training_data)
     np.save('train_data.npy',training_data)
     return training_data
+
+# crete testing data
+def create_test_data():
+    testing_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
+        img_num = img.split('.')[0]
+        img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
+        testing_data.append([np.array(img),img_num])
+    np.save('test_data.npy',testing_data)
+    return testing_data
+
